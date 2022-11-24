6f07745aac9aab8e06a301c7f12ef94abf330821
Daniel Volosov <45984308+danielvolosov@users.noreply.github.com>
2022-11-23 23:49:48 -0500

    Enable city search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-
+import { GEO_API_URL, geoApiOptions } from "../../services/api";
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
@@ -9,13 +9,30 @@ const Search = ({onSearchChange}) => {
         setSearch(searchData);
         onSearchChange(searchData);
     }
-    
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions)
+        .then(response => response.json())
+        .then(response => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name} ${city.countryCode}`
+                    }
+                })
+            }
+        })
+        .catch(err => console.error(err));
+    }
+
     return (
         <AsyncPaginate 
             placeholder="Search for city"
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
